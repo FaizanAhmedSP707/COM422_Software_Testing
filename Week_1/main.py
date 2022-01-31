d898cb5ae4bf514a17ab2bbf72cb1d4a30590527
Faizan Ahmed <5ahmef94@solent.ac.uk>
2022-01-31 23:27:45 +0000

    Modified main.py to cope with the amendment to the accelerate method (Q1).

--- Week_1/main.py
+++ Week_1/main.py
@@ -2,10 +2,32 @@
 
 from Car import Car
 
-car1 = Car('Toyota', 'Camry', 'SE', 2.0, 'Silver', 'FWD', 'Automatic',
-           46, 220, 64)
-car2 = Car('Honda', 'Accord', 'Limited', 3.5, 'Beige', 'RWD', 'Manual',
-           100, 260, 87)
+if __name__ == '__main__':
+    car1 = Car('Toyota', 'Camry', 'SE', 2.0, 'Silver', 'FWD', 'Automatic', 46, 220, 64)
+    car2 = Car('Honda', 'Accord', 'Limited', 3.5, 'Beige', 'RWD', 'Manual', 100, 260, 87)
 
-print(f"Car 1 is moving at {car1.currentSpeed}kph with {car1.fuelLevel}% fuel left in the tank.")
-print(f"Car 2 is moving at {car2.currentSpeed}kph with {car2.fuelLevel}% fuel left in the tank.")
+    print(f"Car 1 is moving at {car1.currentSpeed}kph with {car1.fuelLevel}% fuel left in the tank.")
+    print(f"Car 2 is moving at {car2.currentSpeed}kph with {car2.fuelLevel}% fuel left in the tank.\n")
+
+    car1.accelerate(26)
+    car2.brake()
+
+    print(f"Car 1 is moving at this speed now: {car1.currentSpeed}kph with fuel qty: {car1.fuelLevel}%.")
+    print(f"Car 2 is moving at this speed now: {car2.currentSpeed}kph with fuel qty: {car2.fuelLevel}%.\n")
+
+    # Q1 of improving the methods
+    while True:
+        car_select_opt = input("Please select either car1 (type '1') to increase its' speed, or car 2 (type '2'): ")
+        if car_select_opt == '1':
+            accelerate_by = int(input("Please enter a value for which the speed of the car will increase by:\n"))
+            car1.accelerate(accelerate_by)
+            print(f"Car 1's current speed is : {car1.currentSpeed}kph.")
+            break
+        elif car_select_opt == '2':
+            accelerate_by = int(input("Please enter a value for which the speed of the car will increase by:\n"))
+            car2.accelerate(accelerate_by)
+            print(f"Car 2's current speed is : {car2.currentSpeed}kph.")
+            break
+        else:
+            print("Exiting loop, returning....")
+            break
