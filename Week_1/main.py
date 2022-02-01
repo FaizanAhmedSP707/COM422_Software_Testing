6271413b42fd318a73901b073b21e1adee27fd8c
Faizan Ahmed <5ahmef94@solent.ac.uk>
2022-02-01 10:09:22 +0000

    Modified this file to cope with the amendment to the brake method (Q2) + added the check for Q4.

--- Week_1/main.py
+++ Week_1/main.py
@@ -9,13 +9,14 @@ if __name__ == '__main__':
     print(f"Car 1 is moving at {car1.currentSpeed}kph with {car1.fuelLevel}% fuel left in the tank.")
     print(f"Car 2 is moving at {car2.currentSpeed}kph with {car2.fuelLevel}% fuel left in the tank.\n")
 
-    car1.accelerate(26)
-    car2.brake()
+    # EARLIER DEBUGGING PURPOSE, COMMENTED OUT RIGHT NOW
+    # car1.accelerate(26)
+    # car2.brake(27)
 
     print(f"Car 1 is moving at this speed now: {car1.currentSpeed}kph with fuel qty: {car1.fuelLevel}%.")
     print(f"Car 2 is moving at this speed now: {car2.currentSpeed}kph with fuel qty: {car2.fuelLevel}%.\n")
 
-    # Q1 of improving the methods
+    # Q1, Q3 and Q5 of improving the methods
     while True:
         car_select_opt = input("Please select either car1 (type '1') to increase its' speed, or car 2 (type '2'): ")
         if car_select_opt == '1':
@@ -27,6 +28,22 @@ if __name__ == '__main__':
             accelerate_by = int(input("Please enter a value for which the speed of the car will increase by:\n"))
             car2.accelerate(accelerate_by)
             print(f"Car 2's current speed is : {car2.currentSpeed}kph.")
+            break
+        else:
+            print("Exiting loop, returning....")
+            break
+
+    while True:
+        user_select_opt = input("Please select either car1 (type '1') to decrease its' speed, or car 2 (type '2'): ")
+        if user_select_opt == '1':
+            decelerate_by = int(input("Please enter a value for which the speed of the car will decrease by:\n"))
+            car1.brake(decelerate_by)
+            print(f"Car 1's current speed is {car1.currentSpeed}kph.")
+            break
+        elif user_select_opt == '2':
+            decelerate_by = int(input("Please enter a value for which the speed of the car will decrease by:\n"))
+            car2.brake(decelerate_by)
+            print(f"Car 2's current speed is {car2.currentSpeed}kph.")
             break
         else:
             print("Exiting loop, returning....")
